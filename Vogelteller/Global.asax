f939d4b1006c7a686f452888c232c49dc928f7e8
Roger <Roger@DUNSPARCE>
2018-03-23 12:56:48 +0100

    Add project files.

---
+++ Vogelteller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vogelteller.MvcApplication" Language="C#" %>
---
+++ Vogelteller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vogelteller.Startup))]
+namespace Vogelteller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

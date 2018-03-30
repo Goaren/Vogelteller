08983933547b7a257bfc0dbdd5885243f33bbeef
Roger <tunero@live.nl>
2018-03-30 18:11:29 +0200

    TEST 1

---
+++ Models/Models/Bezoek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    class Bezoek
+    {
+        private int id;
+        private DateTime start;
+        private DateTime einde;
+
+        public int Id { get { return id; } set { id = value; } }
+        public DateTime Start { get { return start; } set { start = value; } }
+        public DateTime Einde { get { return einde; } set { einde = value; } }
+
+        public Bezoek(int id, DateTime start, DateTime einde)
+        {
+            this.id = id;
+            this.start = start;
+            this.einde = einde;
+        }
+    }
+}
---
+++ Models/Models/Waarneming.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    class Waarneming
+    {
+        private int id;
+        private double x;
+        private double y;
+        private DateTime date;
+
+        public int Id { get { return id;  } set { id = value;  } }
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+
+        public Waarneming(int id, double x, double y, DateTime date)
+        {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.date = date;
+        }
+    }
+}
